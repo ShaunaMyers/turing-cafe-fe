c16932e5d68b3dac2861723b50423a9926e9ec60
Shauna Myers <shaunamyers26@gmail.com>
2021-07-21 16:50:30 +0100

    Import Reservation component and map over reservations

--- src/components/ResContainer/ResContainer.js
+++ src/components/ResContainer/ResContainer.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import Reservation from '../Reservation/Reservation';
 
 const ResContainer = (props) => {
+
+    props.reservations.map(reservation => {
+        return <Reservation />
+    })
+
     return ( 
         <h2>Hi</h2>
      );
