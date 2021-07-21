3073933c3b1fc8b0e7023461421e7adec8691228
Shauna Myers <shaunamyers26@gmail.com>
2021-07-21 18:05:09 +0100

    Write tests to check for inputs with placeholder values

--- cypress/integration/main_page_spec.js
+++ cypress/integration/main_page_spec.js
@@ -1,8 +1,35 @@
 describe('Main Reservation Page', () => {
+
+    beforeEach(() => {
+        cy 
+        .visit('http://localhost:3000/')
+    })
+
+    it('Should be able to display a header when the page is visited', () => {
+        cy 
+        .get('h1')
+        .contains('Turing Cafe Reservations')
+    })
+
+    it('Should be able to display an input to type a name', () => {
+        cy 
+        .get('form')
+        .get('input').first()
+        .invoke('attr', 'placeholder')
+        .should('contain', 'Your Name')
+    })
+
+    it('Should be able to display an input to type a date', () => {
+        cy 
+        .get('form')
+        .get('input').next()
+        .invoke('attr', 'placeholder')
+        .should('contain', 'Date')
+    })
+
     it('Should be able to display all reservations when the page is visited', () => {
         cy 
-            .visit('http://localhost:3000/')
-            .get('.resCard')
-            .should('have.length', 9)
+        .get('.resCard')
+        .should('have.length', 9)
     })
 })
