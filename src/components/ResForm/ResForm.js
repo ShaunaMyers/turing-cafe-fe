14fdddfb66c4406da2565ab29f4a37527e3cc18a
Shauna Myers <shaunamyers26@gmail.com>
2021-07-21 17:26:19 +0100

    Fix missing this. in front of method calls and change Form to form

--- src/components/ResForm/ResForm.js
+++ src/components/ResForm/ResForm.js
@@ -29,13 +29,13 @@ class ResForm extends Component {
 
     render() { 
         return ( 
-            <Form>
-                <input onChange={handleAddName} type="text" placeholder="Your Name" value={this.state.name}/>
-                <input onChange={handleAddDate} type="text" placeholder="Date" value={this.state.date}/>
-                <input onChange={handleAddTime} type="text" placeholder="Time" value={this.state.time}/>
-                <input onChange={handleAddNumber} type="text" placeholder="Number of People in Group" value={this.state.number}/>
+            <form>
+                <input onChange={this.handleAddName} type="text" placeholder="Your Name" value={this.state.name}/>
+                <input onChange={this.handleAddDate} type="text" placeholder="Date" value={this.state.date}/>
+                <input onChange={this.handleAddTime} type="text" placeholder="Time" value={this.state.time}/>
+                <input onChange={this.handleAddNumber} type="text" placeholder="Number of People in Group" value={this.state.number}/>
                 <button>Make Reservation</button>
-            </Form>
+            </form>
          );
     }
 }
