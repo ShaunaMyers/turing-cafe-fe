d2813e45c6ae1538d28806853efacc81626c07a9
Shauna Myers <shaunamyers26@gmail.com>
2021-07-21 17:17:20 +0100

    Build basic structure of class component for ResForm

---
+++ src/components/ResForm/ResForm.js
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+
+class ResForm extends Component {
+    constructor() {
+        super();
+        this.state = { 
+            name: '',
+            date: '',
+            time: '',
+            number: ''
+         }
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default ResForm;
