95e7413c2b76514e4c63f95cb1a70c61acae43aa
Shauna Myers <shaunamyers26@gmail.com>
2021-07-21 17:21:11 +0100

    Add attributes to inputs

--- src/components/ResForm/ResForm.js
+++ src/components/ResForm/ResForm.js
@@ -13,10 +13,10 @@ class ResForm extends Component {
     render() { 
         return ( 
             <Form>
-                <input onChange={handleAddName}/>
-                <input onChange={handleAddDate}/>
-                <input onChange={handleAddTime}/>
-                <input onChange={handleAddNumber}/>
+                <input onChange={handleAddName} type="text" placeholder="Your Name"/>
+                <input onChange={handleAddDate} type="text" placeholder="Date"/>/>
+                <input onChange={handleAddTime} type="text" placeholder="Time"/>/>
+                <input onChange={handleAddNumber} type="text" placeholder="Number of People in Group"/>/>
                 <button>Make Reservation</button>
             </Form>
          );
